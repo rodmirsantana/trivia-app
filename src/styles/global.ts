e05ceca2ba495a5ca000bb96b692b64aa1aa887e
rodmirsantana <rodmirandasantana@gmail.com>
2021-10-27 18:23:34 -0300

    Init project setup

---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle, css, DefaultTheme } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle<{ theme: DefaultTheme }>`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  ${({ theme }) => css`
+    html {
+      font-size: 62.5%;
+    }
+
+    body {
+      font-family: ${theme.font.family};
+      font-size: ${theme.font.sizes.medium};
+    }
+  `}
+`
+
+export default GlobalStyle
